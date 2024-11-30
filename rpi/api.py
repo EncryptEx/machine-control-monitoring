04c69803ba9d9a68cadc2df4961b6f6214d81445
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2024-11-30 23:54:53 +0100

    velocity not working

--- rpi/api.py
+++ rpi/api.py
@@ -1,7 +1,13 @@
+import threading
+import time
+import logging
 from fastapi import FastAPI
 import RPi.GPIO as GPIO
 
 app = FastAPI()
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 # configure Swagger
 app.title = "Machine Control Monitoring API"
@@ -19,7 +25,8 @@ GPIO.setup(IR_LED, GPIO.IN)
 p = GPIO.PWM(servoPIN, 50)
 areMotorsEnabled = False
 servoSpeed = 0
-
+realvelocity_value = 0.0
+totalBoxesCount = 0
 
 # Helper function to normalize values
 def normalize_to_duty_cycle(value: float) -> float:
@@ -37,8 +44,40 @@ def normalize_to_duty_cycle(value: float) -> float:
         duty_cycle = neutral_duty + (12 - neutral_duty) * value / 5
     
     return duty_cycle
+
+def compute_velocity():
+    # calc time between two rising edges of IR 
+    global realvelocity_value
+    # wait for a rising edge
+    while GPIO.input(IR_LED) == 1:
+        pass
+    time.sleep(0.01)  # Add a small debounce delay
+    while GPIO.input(IR_LED) == 0:
+        pass
+    start = time.perf_counter()
+    while GPIO.input(IR_LED) == 1:
+        pass
+    time.sleep(0.01)  # Add a small debounce delay
+    while GPIO.input(IR_LED) == 0:
+        pass
+    
+    end = time.perf_counter()
+    # calc velocity knowing that 73mm / time = velocity
+    realvelocity_value = 0.073 / (end - start) # m/s
+    time.sleep(0.1)
+    
+    
+    
+    
+
+# Start the frequency computation in a separate thread
+frequency_thread = threading.Thread(target=compute_velocity)
+frequency_thread.daemon = True
+frequency_thread.start()
+
 @app.get("/helloworld")
 def helloworld():
+    logger.debug("Handling root endpoint request")
     return {"Hello": "World"}
 
 @app.get("/motors/enable/")
@@ -68,6 +107,9 @@ def enable_motors(value: float):
     return {'success': True, 'message': f'Motors enabled at normalized value {value} (Duty cycle: {duty_cycle})'}
 
 
+def readNormalizedIR():
+    return 1 if GPIO.input(IR_LED) == 0 else 0
+
 @app.get("/motors/disable")
 def disable_motors():
     """
@@ -95,10 +137,18 @@ def readIR():
 def read():
     # prepare the response of all the pins
     response = {
-        'IR_LED': GPIO.input(IR_LED),
+        'IR_LED': readNormalizedIR(),
         'isServoRunning': areMotorsEnabled,
         'normalizedServoSpeed': servoSpeed,
-        'realServoDutyCycle': normalize_to_duty_cycle(servoSpeed)
+        'realServoDutyCycle': normalize_to_duty_cycle(servoSpeed),
+        'realVelocity': realvelocity_value,
+        'totalBoxesCount': totalBoxesCount,
+        'timestamp': (int)(time.time())
     }
 
     return response
+
+# compute frequency between 1 rising edge and 1 falling edge
+@app.get("/velocity")
+def get_velocity():
+    return {"velocity": realvelocity_value}
