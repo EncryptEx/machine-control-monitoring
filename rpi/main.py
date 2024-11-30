72557179091b540c536fc46492099effc313b57c
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2024-11-30 17:30:45 +0100

    YAAAAY ITS WORKING

--- rpi/main.py
+++ rpi/main.py
@@ -1,118 +1,30 @@
-import gpiod
-
+import RPi.GPIO as GPIO
 import time
 
-# Define the control pins
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-ControlPin = [14, 15, 18, 23]
-
-# Create a GPIO chip and get the lines
-
-chip = gpiod.Chip('gpiochip4')
-
-lines = [chip.get_line(pin) for pin in ControlPin]
-
-# Configure the lines as outputs
-
-for line in lines:
-
-   line.request('stepper_motor', gpiod.LINE_REQ_DIR_OUT, 0)
-
-# Define the segment pattern
-
-seg_right_full = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-]
-
-seg_left_full = [
-    [0, 0, 0, 1],
-    [0, 0, 1, 0],
-    [0, 1, 0, 0],
-    [1, 0, 0, 0]
-]
-
-
-
-seg_right = [
-
-   [1, 0, 0, 0],
-
-   [1, 1, 0, 0],
-
-   [0, 1, 0, 0],
-
-   [0, 1, 1, 0],
-
-   [0, 0, 1, 0],
-
-   [0, 0, 1, 1],
-
-   [0, 0, 0, 1],
-
-   [1, 0, 0, 1]
-
-]
-
-seg_left = [
-
-   [0, 0, 0, 1],
-
-   [0, 0, 1, 1],
-
-   [0, 0, 1, 0],
-
-   [0, 1, 1, 0],
-
-   [0, 1, 0, 0],
-
-   [1, 1, 0, 0],
-
-   [1, 0, 0, 0],
-
-   [1, 0, 0, 1]
-
-]
-
-
-halfstepEnabled = True
-
-delay = 0.001 #You can control the acceleration from the delay value !
-
-# Run the stepper motor for 512 steps
-
-for i in range(512):
-
-    if(halfstepEnabled):
-        for halfstep in range(8):
-            for pin in range(4):
-                lines[pin].set_value(seg_right[halfstep][pin])
-            time.sleep(delay)
-
-    else: 
-        for pin in range(4):
-            lines[pin].set_value(seg_right_full[i%4][pin])
-        time.sleep(delay)
-
-
-for i in range(512):
-    if(halfstepEnabled):
-        for halfstep in range(8):
-            for pin in range(4):
-                lines[pin].set_value(seg_left[halfstep][pin])
-            time.sleep(delay)
-
-    else: 
-        for pin in range(4):
-            lines[pin].set_value(seg_left_full[i%4][pin])
-        time.sleep(delay)
-
-# Release the GPIO lines
-
-for line in lines:
-
-   line.release()
-
-
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
